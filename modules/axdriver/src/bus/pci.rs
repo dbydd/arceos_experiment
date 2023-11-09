14b1a070e1ac0f34da158694343b7cbc70015a7f
dbydd <1992003927@qq.com>
2023-11-09 17:52:46 +0800

    need to modify modules/axdriver/src/bus/pci.rs to fix memory alloc

--- modules/axdriver/src/bus/pci.rs
+++ modules/axdriver/src/bus/pci.rs
@@ -99,24 +99,24 @@ impl AllDevices {
                 if dev_info.header_type != HeaderType::Standard {
                     continue;
                 }
-                match config_pci_device(&mut root, bdf, &mut allocator) {
-                    Ok(_) => for_each_drivers!(type Driver, {
-                        if let Some(dev) = Driver::probe_pci(&mut root, bdf, &dev_info) {
-                            info!(
-                                "registered a new {:?} device at {}: {:?}",
-                                dev.device_type(),
-                                bdf,
-                                dev.device_name(),
-                            );
-                            self.add_device(dev);
-                            continue; // skip to the next device
-                        }
-                    }),
-                    Err(e) => warn!(
-                        "failed to enable PCI device at {}({}): {:?}",
-                        bdf, dev_info, e
-                    ),
-                }
+                // match config_pci_device(&mut root, bdf, &mut allocator) {
+                //     Ok(_) => for_each_drivers!(type Driver, {
+                //         if let Some(dev) = Driver::probe_pci(&mut root, bdf, &dev_info) {
+                //             info!(
+                //                 "registered a new {:?} device at {}: {:?}",
+                //                 dev.device_type(),
+                //                 bdf,
+                //                 dev.device_name(),
+                //             );
+                //             self.add_device(dev);
+                //             continue; // skip to the next device
+                //         }
+                //     }),
+                //     Err(e) => warn!(
+                //         "failed to enable PCI device at {}({}): {:?}",
+                //         bdf, dev_info, e
+                //     ),
+                // } //todo fix memory alloc
             }
         }
     }
